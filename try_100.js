a9f399a6b9d536cd0c1a9bb319b127eec90fa405
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 11:25:25 +0300

    this code is not finished

--- try_100.js
+++ try_100.js
@@ -1,25 +1,24 @@
-Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again. The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+//This codes anables the use to enter a number that is grator than 100
 
->> At first, we can assume that the visitor only inputs numbers and there’s no need to implement a special handling for a non-numeric input in this task.
-
-++ As a bonus, Can you add that feature to your code.
 //Pseudo code
 /*
 ______________________________________________________________________________
 Initialise a varible to hold the number that will be iniput by the user
 Prompt the user to supply the number
-While number is less than 100
+While number is less than or equal to 100 and input is not empty
 do...i.e ask the user to try again
 
 ______________________________________________________________________________
 */
 
-do {
-// var input = "Enter a number grator than 100"
-var input = 100; //assume he entered 100
-}
-While (input<=100)
+
+
+// var input = "Enter a number grator than 100" //LINE 6 and 7
+
+var input =101 ; //assume he entered 101
+
+while (input !==null && input<=100)
 {
-	console.log ("It should be greater than 100");
-	
+ //if true ask again
+ //if false print the number and stop
 }
