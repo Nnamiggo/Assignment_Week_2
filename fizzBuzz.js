41e85ef480f2357ef1d9280ef93d2e79d48a1fc8
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 13:17:18 +0300

    coding

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,16 @@
-
-
-Write a program that prints the numbers from 1 to 100 (you can use a for loop or while loop here); 
-But for multiples of three, print "Fizz"instead of the number, and for the multiples of five, print "Buzz".
- For numbers which are multiples of both three and five, print "FizzBuzz".
  // The fizzbuzz project
+ 
+ //Pseudo code
+ /*
+ ________________________________________________________________________
+ 1. Create a for loop initialize the value of the counter i to 1 condition less or equal to 100 increment by 1
+ 2. If i is divisible by both 3 and 5 print FizzBuzz
+ 3. If it is divisible by 3 print Fizz
+ 4. If it is divisible by 5 print Buzz
+ 5. Else print i
+ ________________________________________________________________________
+ */
+ for (var i=1; i<=100; i++)
+ {
+
+ }
