29b4c4ac322df0e24b6436cfb5db4a8872187551
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 06:16:44 +0300

    creating file understanding instructions

---
+++ ageCalculator.js
@@ -0,0 +1,3 @@
+Write a program that takes in a number as input inform of a user's date of birth in this format, "mm-dd-yyyy" and prints the age of the user to the terminal.
+
+For example, If the input is 03-23-1990, it should return: You are 29 years old
