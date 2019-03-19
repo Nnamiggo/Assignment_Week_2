9cb0c461b172c686702743fe27612c0b39cf0c43
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 05:32:46 +0300

    creating pseudo code

--- Src/billing.js
+++ Src/billing.js
@@ -1,13 +1,12 @@
 /*Create your project structure and complete assignment
 
-In this assignment, you are going to create a simple billing calculator that deducts transaction charges from the amount tendered by a customer. The assignment will test your understanding of the concepts covered so far.
 
-    To start, open the terminal on your computer, change to the directory where you want to store this assignment and create a new directory named "Assignment_Week_2"
-    Change your current directory to the newly created directory "Assignment_Week_2"
-    Inside of "Assignment_Week_2" directory, create another directory called Src and create a file called billing.js using the touch command or with your favorite text editor
-    In src/billing.js create a constant called transactionCharge and set it equal to 2.5%
-    In the line before the creation of the constant (line 1), leave a single line comment This is the percentage that determines the transaction charge
+
+
+    In the line before the creation of the constant (line 1), leave a single line comment 
+
     Create a variable amountTendered and set it equal to 100000
+    
     Use console.log to print the following message: 'This is what the billed amount is (less transaction charges);'
     On the next line - again, use the console.log to print the result of the billed amount is which is as a result of subtracting the transaction charge from the Amount tendered.
     To run your code, you have to run the following command from your terminal node billing.js
@@ -15,3 +14,15 @@ In this assignment, you are going to create a simple billing calculator that ded
     The result should be as indicated below
     If correct, submit your file billing.js to your github repository under a new repository "Intro to javascript"
     Share the link to your repository in the Slack channel unit_2_days_11-15*/
+
+    //My Pseudo code
+    Declare a constant to hold the (2.5/100) constant for it is a parcentange...after the comment
+    Declare amountTendered variable initialising it to 100000
+    Use console.log to print the following message: 'This is what the billed amount is (less transaction charges);'
+    On the next line - again, use the console.log to print the result of the billed amount is which is as 
+    a result of subtracting the transaction charge from the Amount tendered.
+    submiting tthis file into a new repository called Intro to javascript requires submodules...complex topic!
+
+    // This is the percentage that determines the transaction charge
+       
+
