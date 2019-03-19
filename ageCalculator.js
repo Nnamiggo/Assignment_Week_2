081ac7b2dd549bd0b86cc10941238f68e3dc918f
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 06:31:38 +0300

    fixing errors

--- ageCalculator.js
+++ ageCalculator.js
@@ -9,7 +9,7 @@ Pick the year frolm the retujrned array using indexes and
 subtract it froml the current year 2019 to get the age..........says instructor
 ________________________________________________________________________________________
 */
-var myBD = 11-01-1988; 
-var splitit = split("-");
+var myBD = '11-01-1988'; 
+var splitit = myBD.split("-");
 var myAge = 2019 - splitit[2];
 console.log("Your current age is: " + myAge);
