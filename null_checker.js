a5746e60a201fefb176780c0fdde38f55e022fbe
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 06:47:13 +0300

    creating file and uderstanding coide

---
+++ null_checker.js
@@ -0,0 +1,12 @@
+
+
+Using if..else, write the code which gets a number using prompt and then shows in console.log:
+
+    1, if the value is greater than zero,
+    -1, if less than zero,
+    0, if equals zero.
+
+In this task we assume that the input is always a number.
+
+
+Save the file as null_checker in the same folder that you saved yesterday's work.
