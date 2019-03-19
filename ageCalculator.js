e261513169d27dab84a799ec7ac734a6ea7c24e2
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 06:34:27 +0300

    commenting code

--- ageCalculator.js
+++ ageCalculator.js
@@ -5,11 +5,12 @@ ________________________________________________________________________________
 
 Declare a variable to hold the birth date as a string .......for prompt is forbidden at this stage by instructor
 Use the split function to separate the date and save it in anolther variable
-Pick the year frolm the retujrned array using indexes and 
-subtract it froml the current year 2019 to get the age..........says instructor
+Pick the year from the returned array using indexes and 
+subtract it from the current year 2019 to get the age..........says instructor
+pprint the above result
 ________________________________________________________________________________________
 */
-var myBD = '11-01-1988'; 
-var splitit = myBD.split("-");
-var myAge = 2019 - splitit[2];
-console.log("Your current age is: " + myAge);
+var myBD = '11-01-1988';  //implementi line6
+var splitit = myBD.split("-"); //implementi line7
+var myAge = 2019 - splitit[2]; //implementi line8 n 9
+console.log("Your current age is: " + myAge); //implementi line10
