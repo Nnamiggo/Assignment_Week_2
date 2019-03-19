3bf7aea89d69d980d7f4bfd14463f4086c4c09a1
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 06:22:05 +0300

    writing pseudo code

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,3 +1,15 @@
-Write a program that takes in a number as input inform of a user's date of birth in this format, "mm-dd-yyyy" and prints the age of the user to the terminal.
+Write a program that takes in a number as input inform of a user's date of birth in this format, "mm-dd-yyyy"
+ and prints the age of the user to the terminal.
 
 For example, If the input is 03-23-1990, it should return: You are 29 years old
+//Pseudo code
+
+/*
+_____________________________________________________________________________________
+
+Declare a variable to hold the age as a string .......for prompt is forbidden at this stage by instructor
+Use the split function to separate the date and save it in anolther variable
+Pick the year frolm the retujrned array usikng indexes and 
+subtract it froml the current year 2019 to get the age..........says instructor
+________________________________________________________________________________________
+*/
