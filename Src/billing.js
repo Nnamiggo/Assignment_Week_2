f16645088f6cd531a7fca4e42476d8a44c97c80a
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 05:33:33 +0300

    creating pseudo code

--- Src/billing.js
+++ Src/billing.js
@@ -1,27 +1,13 @@
-/*Create your project structure and complete assignment
-
-
-
-
-    In the line before the creation of the constant (line 1), leave a single line comment 
-
-    Create a variable amountTendered and set it equal to 100000
-    
-    Use console.log to print the following message: 'This is what the billed amount is (less transaction charges);'
-    On the next line - again, use the console.log to print the result of the billed amount is which is as a result of subtracting the transaction charge from the Amount tendered.
-    To run your code, you have to run the following command from your terminal node billing.js
-    It is important that you are within the src directory when implementing the command
-    The result should be as indicated below
-    If correct, submit your file billing.js to your github repository under a new repository "Intro to javascript"
-    Share the link to your repository in the Slack channel unit_2_days_11-15*/
 
     //My Pseudo code
+    /*
     Declare a constant to hold the (2.5/100) constant for it is a parcentange...after the comment
     Declare amountTendered variable initialising it to 100000
     Use console.log to print the following message: 'This is what the billed amount is (less transaction charges);'
     On the next line - again, use the console.log to print the result of the billed amount is which is as 
     a result of subtracting the transaction charge from the Amount tendered.
     submiting tthis file into a new repository called Intro to javascript requires submodules...complex topic!
+    */
 
     // This is the percentage that determines the transaction charge
        
