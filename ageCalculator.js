0febd73b298ae067639ff736f873672146b61e95
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 06:27:37 +0300

    coding

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,15 +1,15 @@
-Write a program that takes in a number as input inform of a user's date of birth in this format, "mm-dd-yyyy"
- and prints the age of the user to the terminal.
-
-For example, If the input is 03-23-1990, it should return: You are 29 years old
 //Pseudo code
 
 /*
 _____________________________________________________________________________________
 
-Declare a variable to hold the age as a string .......for prompt is forbidden at this stage by instructor
+Declare a variable to hold the birth date as a string .......for prompt is forbidden at this stage by instructor
 Use the split function to separate the date and save it in anolther variable
-Pick the year frolm the retujrned array usikng indexes and 
+Pick the year frolm the retujrned array using indexes and 
 subtract it froml the current year 2019 to get the age..........says instructor
 ________________________________________________________________________________________
 */
+var myBD = 11-01-1988; 
+var splitit = split("-");
+var myAge = 2019 - splitit[2];
+console.log("Your current age is: " + myAge);
