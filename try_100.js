6bb80a5688288f062ace057d81028e4a9c1cb059
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 07:51:01 +0300

    writing pseudo code

--- try_100.js
+++ try_100.js
@@ -3,3 +3,13 @@ Write a loop which prompts for a number greater than 100. If the visitor enters 
 >> At first, we can assume that the visitor only inputs numbers and there’s no need to implement a special handling for a non-numeric input in this task.
 
 ++ As a bonus, Can you add that feature to your code.
+//Pseudo code
+/*
+______________________________________________________________________________
+Initialise a varible to hold the number that will be iniput by the user
+Prompt the user to supply the number
+While number is less than 100
+do...i.e ask the user to try again
+
+______________________________________________________________________________
+*/
