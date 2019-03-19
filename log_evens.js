57b7b5e57bb17e88f5c5409716c2ba635a2bbc01
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 07:19:52 +0300

    my pseudo code

--- log_evens.js
+++ log_evens.js
@@ -2,7 +2,7 @@ Use the for loop to output even numbers from 2 to 20.
 //Pseudo code
 /*
 ________________________________________________
-Initialise the counter i to 2
+Initialise the variable counter i in the for loop to 2
 Test whether it is grater than 20
 Increment it by 1 and continue until condition is met
 Test if mudulus of i and 2 is zero
