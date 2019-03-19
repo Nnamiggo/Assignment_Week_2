b67ba54eb0aaa19e584d9d1883eb13d76c0ea99b
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 20:46:14 +0300

    editing code

--- sort_primes.js
+++ sort_primes.js
@@ -12,34 +12,39 @@ var arr = []; //Initialize the array to empty
 
 //when calling the prime_number_calculator function initialise the paramater to  100
 
-prime_number_calculator = () => {
-  for (var i =2; i<=100; i++)
+prime_number_calculator = (x,y) => {
+  for (var i=x; i<=y; i++)
   {
-    var are_you_prime = true;
+    if(i<2) //For 2 is always the first prime number
+     { 
 
-        for (var testing_u = 2; testing_u<=i; testing_u++)
+    var are_you_prime = true; //Assume the number is prime and test if this is really true
+
+        for (var testing_u = 2; testing_u<=i; testing_u++) //initialise counter to 2 because 1 will always divide the number
         {
-          if (i%testing_u==0 && testing_u!==i)
+          if (i%testing_u==0 && testing_u!==i) //If the diviser is not the number
           {
-            are_you_prime =false;
+            are_you_prime =false; //Then the number ceases to be prime
           }
           
         }
+      }
         if (are_you_prime==true )
           {
-            arr.push(i );
+            console.log(i)
+            //arr.push(i );
           }
              } 
 }
-prime_number_calculator(100);
+prime_number_calculator(0,100);
 
 //console.log(arr);
 
-var distorted = disorganize(arr);
+//var distorted = disorganize(arr);
 
 //console.log(distorted);
 
-var x = distorted;
+//var x = distorted;
 
 //get an array element 
 //compare if it is smaller than all other elements
