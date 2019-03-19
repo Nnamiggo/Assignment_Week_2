0db359c3c60213ad04bff29ecbf61cc82863516a
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-19 12:55:06 +0300

    understanding the problem and creating the file

---
+++ fizzBuzz.js
@@ -0,0 +1,6 @@
+
+
+Write a program that prints the numbers from 1 to 100 (you can use a for loop or while loop here); 
+But for multiples of three, print "Fizz"instead of the number, and for the multiples of five, print "Buzz".
+ For numbers which are multiples of both three and five, print "FizzBuzz".
+ // The fizzbuzz project
